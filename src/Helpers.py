9f60c55e89c7fbd8ced042d77b29847e4b1611f8
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2023-06-27 16:57:19 +0200

    add random sample df creation to helpers

--- src/Helpers.py
+++ src/Helpers.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 
 def convert_probabilities_to_decimal(probabilities, decimal_places=8):
@@ -10,3 +11,9 @@ def convert_probabilities_to_decimal(probabilities, decimal_places=8):
 
 def standardise_deberta(deberta_results):
     return [[result[0], result[2], result[1]] for result in deberta_results]
+
+
+def get_random_samples(csv_filename, num_samples):
+    df = pd.read_csv(csv_filename)
+    random_samples = df.sample(n=num_samples)
+    return random_samples
