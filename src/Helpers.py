8c48e1d4fd847252571bc29d0f4305d1417cdd01
AlfonsoGhislieri <alfonso.ghis@hotmail.fr>
2023-06-28 15:08:42 +0200

    add benchmark test

--- src/Helpers.py
+++ src/Helpers.py
@@ -1,5 +1,6 @@
-import numpy as np
 import pandas as pd
+import torch
+import time
 
 
 def convert_probabilities_to_decimal(probabilities, decimal_places=8):
@@ -17,3 +18,21 @@ def get_random_samples(csv_filename, num_samples):
     df = pd.read_csv(csv_filename)
     random_samples = df.sample(n=num_samples)
     return random_samples
+
+
+def benchmark_test(premise, hypothesis, model, num_runs):
+    # Setup device
+    if torch.cuda.is_available():
+        device = torch.device("cuda")
+    else:
+        device = torch.device("cpu")
+
+    results = []
+    for _ in range(num_runs):
+        start_time = time.time()
+        _, _ = model(premise, hypothesis)
+        execution_time = time.time() - start_time
+        results.append(execution_time)
+
+    avg_execution_time = sum(results) / len(results)
+    return device, avg_execution_time
